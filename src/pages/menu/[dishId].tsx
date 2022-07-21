452567a417d8dd96b2a4ba80548f9842cd83c89b
XianFengWu1996 <shawnwu1996@hotmail.com>
2022-07-20 23:28:44 -0400

    create menu items

--- src/pages/menu/[dishId].tsx
+++ src/pages/menu/[dishId].tsx
@@ -1,8 +1,11 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Button, TextField, Typography } from '@mui/material'
+import { isEmpty } from 'lodash'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
+import { GoFlame } from 'react-icons/go'
 import { AppBarNav } from '../../modules/appbar/appbar'
+import { MenuItem } from '../../modules/menu/item/menuItem'
 import { useAppSelector } from '../../store/hook'
 
 export const DishById = () => {
@@ -16,34 +19,14 @@ export const DishById = () => {
         return dish.id === dishId
       })
       setDish(found_dish);
-  }, [])
+  }, [dishId, listOfDishes])
 
   return <>
     <AppBarNav />
 
-    <Box sx={{ 
-        backgroundColor: 'background.paper',
-        minHeight: 'calc(100vh - 68px)',
-    }}>
-      {
-        dish && <div style={{ display: 'flex'}}>
-          <Image 
-            src={dish.pic_url ?? ''} 
-            alt={`Picture of ${dish.en_name}`} 
-            width={300}
-            height={300}
-          />
-
-          <div>
-            <Typography>{dish.label_id}.{dish.en_name} {dish.ch_name}</Typography>
-            <Typography>${dish.price}</Typography>
-
-            <Typography>Description: {dish.description}</Typography>
-          </div>
-
-        </div>
-      }
-    </Box>
+    {
+      dish && <MenuItem dish={dish}/>
+    }
   </>
 }
 
